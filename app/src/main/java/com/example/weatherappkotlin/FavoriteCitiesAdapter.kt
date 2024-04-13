36a78b911c9841bd15fe44e6f06da2f50c4d588f
Jakub Kozlowski <jakub.kozlowski1104@gmail.com>
2024-04-13 14:43:23 +0200

    delete favotites updating

--- app/src/main/java/com/example/weatherappkotlin/FavoriteCitiesAdapter.kt
+++ app/src/main/java/com/example/weatherappkotlin/FavoriteCitiesAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.weatherappkotlin
 
+
 import android.content.Context
 import android.content.SharedPreferences
 import android.view.LayoutInflater
@@ -9,40 +10,20 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-interface CityDeleteListener {
-    fun onCityDeleted(cityName: String)
-}
 class FavoriteCitiesAdapter(
     private val context: Context,
     private val citiesList: MutableList<String>,
-    private val sharedPreferences: SharedPreferences,
-    private val cityDeleteListener: CityDeleteListener
+    private val sharedPreferences: SharedPreferences
 ) : RecyclerView.Adapter<FavoriteCitiesAdapter.CityViewHolder>() {
-
-    private val editor: SharedPreferences.Editor = sharedPreferences.edit()
-
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CityViewHolder {
         val view = LayoutInflater.from(context).inflate(R.layout.item_city, parent, false)
         return CityViewHolder(view)
     }
 
-
     override fun onBindViewHolder(holder: CityViewHolder, position: Int) {
         val city = citiesList[position]
-        holder.bind(city) { clickedCity ->
-            // Usuwamy miasto z listy ulubionych w SharedPreferences
-            val citySet = sharedPreferences.getStringSet("favoriteCities", HashSet())?.toMutableSet()
-            citySet?.remove(clickedCity)
-            editor.putStringSet("favoriteCities", citySet)
-            editor.apply()
-
-            // Usuwamy miasto z RecyclerView
-            citiesList.toMutableList().apply {
-                remove(clickedCity)
-                notifyDataSetChanged()
-            }
-        }
+        holder.bind(city)
     }
 
     override fun getItemCount(): Int {
@@ -53,14 +34,20 @@ class FavoriteCitiesAdapter(
         private val cityNameTextView: TextView = itemView.findViewById(R.id.cityNameTextView)
         private val deleteButton: Button = itemView.findViewById(R.id.deleteButton)
 
-        fun bind(cityName: String, clickListener: (String) -> Unit) {
+        fun bind(cityName: String) {
             cityNameTextView.text = cityName
             deleteButton.setOnClickListener {
-                clickListener(cityName)
+                // Usuwamy miasto z listy ulubionych w SharedPreferences
+                val editor = sharedPreferences.edit()
+                val citySet = sharedPreferences.getStringSet("favoriteCities", HashSet())?.toMutableSet()
+                citySet?.remove(cityName)
+                editor.putStringSet("favoriteCities", citySet)
+                editor.apply()
+
+                // Usuwamy miasto z listy wyświetlanej w RecyclerView
+                citiesList.remove(cityName)
+                notifyDataSetChanged()
             }
         }
     }
-
-
-
 }
--- app/src/main/java/com/example/weatherappkotlin/FavouritesCitiesActivity.kt
+++ app/src/main/java/com/example/weatherappkotlin/FavouritesCitiesActivity.kt
@@ -11,22 +11,27 @@ import androidx.recyclerview.widget.RecyclerView
 class FavouritesCitiesActivity : AppCompatActivity() {
 
     private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: FavoriteCitiesAdapter
+    private lateinit var sharedPreferences: SharedPreferences
+    private var favoriteCitiesList: MutableList<String> = mutableListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_favourite_cities)
 
         recyclerView = findViewById(R.id.recyclerView)
-        val sharedPreferences = getSharedPreferences("FavoriteCitiesPrefs", Context.MODE_PRIVATE)
+        sharedPreferences = getSharedPreferences("FavoriteCitiesPrefs", Context.MODE_PRIVATE)
         val favoriteCitiesSet: Set<String>? = sharedPreferences.getStringSet("favoriteCities", HashSet())
 
-        // Konwertujemy Set<String> na List<String> dla adaptera RecyclerView
-        val favoriteCitiesList: List<String> = favoriteCitiesSet?.toList() ?: listOf()
+        // Konwertujemy Set<String> na MutableList<String> dla adaptera RecyclerView
+        favoriteCitiesList = favoriteCitiesSet?.toMutableList() ?: mutableListOf()
 
-        // Utworzenie i ustawienie adaptera dla RecyclerView
-        val adapter = FavoriteCitiesAdapter(this, favoriteCitiesList, sharedPreferences)
+        setupRecyclerView()
+    }
+
+    private fun setupRecyclerView() {
+        adapter = FavoriteCitiesAdapter(this, favoriteCitiesList, sharedPreferences)
         recyclerView.adapter = adapter
-
         recyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
