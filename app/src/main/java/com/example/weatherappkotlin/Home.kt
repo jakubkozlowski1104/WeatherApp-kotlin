7e3c9b5794fff0d56f3cbf84f86ac1e647dede4d
Jakub Kozlowski <jakub.kozlowski1104@gmail.com>
2024-04-14 22:39:45 +0200

    fetch depend on internet

--- app/src/main/java/com/example/weatherappkotlin/Home.kt
+++ app/src/main/java/com/example/weatherappkotlin/Home.kt
@@ -18,6 +18,7 @@ import java.io.IOException
 class Home : Fragment() {
     private lateinit var binding: FragmentHomeBinding
     private lateinit var networkConnection: NetworkConnection
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -29,108 +30,47 @@ class Home : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val sharedPreferences = activity?.getSharedPreferences("CityWeatherPrefs", Context.MODE_PRIVATE)
+        val cityName = sharedPreferences?.getString("cityName", "") ?: ""
+
         networkConnection = NetworkConnection(requireContext())
         networkConnection.observe(viewLifecycleOwner, Observer { isConnected ->
             if (isConnected) {
                 Log.d("HomeFragmentNet", "Internet connected")
+                // Pobierz dane z API, gdy jest dostęp do Internetu
                 binding.networkStatusTextView.visibility = View.GONE
+                if (cityName.isNotEmpty()) {
+                    fetchWeatherData(cityName)
+                }
             } else {
                 Log.d("HomeFragmentNet", "No internet connection")
                 binding.networkStatusTextView.visibility = View.VISIBLE
+                // Odczytaj dane z SharedPreferences, gdy brak dostępu do Internetu
+                val sharedPreferences = activity?.getSharedPreferences(cityName, Context.MODE_PRIVATE)
+                val temperature = sharedPreferences?.getString("temperature", "")?.toDoubleOrNull() ?: 0.0
+                val pressure = sharedPreferences?.getString("pressure", "")?.toDoubleOrNull() ?: 0.0
+                val description = sharedPreferences?.getString("description", "") ?: ""
+                val lon = sharedPreferences?.getString("lon", "")?.toDoubleOrNull() ?: 0.0
+                val lat = sharedPreferences?.getString("lat", "")?.toDoubleOrNull() ?: 0.0
+                val windSpeed = sharedPreferences?.getString("windSpeed", "")?.toDoubleOrNull() ?: 0.0
+                val windDirection = sharedPreferences?.getString("windDirection", "")?.toDoubleOrNull() ?: 0.0
+
+                Log.d("HomeFragmentNet", "Shared")
+                // Wyświetl dane z SharedPreferences
+                displayWeatherData(cityName, temperature, pressure, description, lon, lat, windSpeed, windDirection)
             }
         })
 
-        val sharedPreferences = activity?.getSharedPreferences("CityWeatherPrefs", Context.MODE_PRIVATE)
-        val cityName = sharedPreferences?.getString("cityName", "") ?: ""
-        Log.d("HomeFragmentCheck", "Odczytano nazwę miasta: $cityName")
-        loadWeatherDataFromSharedPreferences(cityName);
-
-        if (cityName.isNotEmpty()) {
-            fetchWeatherData(cityName)
-        }
-
-        // Poprawna metoda do odnalezienia przycisku za pomocą View Binding
+        // Dodaj obsługę kliknięcia przycisku
         binding.btnAddToFavourite.setOnClickListener {
-            // Pobranie nazwy miasta z SharedPreferences lub z innego źródła
             val cityName = binding.cityNameTextView.text.toString()
             addCityToFavorites(cityName)
             Toast.makeText(context, "Dodano do ulubionych: $cityName", Toast.LENGTH_SHORT).show()
         }
-
-        // Pobierz listę ulubionych miast
-        val favoriteCitiesSet = sharedPreferences?.getStringSet("favoriteCities", HashSet()) ?: HashSet()
-        Log.d("HomeFragmentCheck", "Ulubione miasta: $favoriteCitiesSet")
     }
-    override fun onDestroyView() {
-        super.onDestroyView()
-        // Zwolnij zasoby, np. odłącz obserwację od networkConnection
-        networkConnection.removeObservers(viewLifecycleOwner)
-    }
-
-
-    private fun addCityToFavorites(cityName: String) {
-        val sharedPreferences = activity?.getSharedPreferences("FavoriteCitiesPrefs", Context.MODE_PRIVATE)
-        val editor = sharedPreferences?.edit()
-
-        // Pobranie aktualnej listy ulubionych miast
-        val favoriteCities = sharedPreferences?.getStringSet("favoriteCities", HashSet())?.toMutableSet() ?: mutableSetOf()
-
-        // Dodanie nowego miasta do listy
-        favoriteCities.add(cityName)
-
-        // Zapisanie zaktualizowanej listy do SharedPreferences
-        editor?.putStringSet("favoriteCities", favoriteCities)?.apply()
-        Log.d("FavoriteCitiesXD", "Ulubione miasta: $favoriteCities")
-    }
-
-
-    private fun loadWeatherDataFromSharedPreferences(cityName: String) {
-        val sharedPreferences = activity?.getSharedPreferences(cityName, Context.MODE_PRIVATE)
-
-        val temperature = sharedPreferences?.getString("temperature", "")?.toDoubleOrNull() ?: 0.0
-        val pressure = sharedPreferences?.getString("pressure", "")?.toDoubleOrNull() ?: 0.0
-        val description = sharedPreferences?.getString("description", "") ?: ""
-        val lon = sharedPreferences?.getString("lon", "")?.toDoubleOrNull() ?: 0.0
-        val lat = sharedPreferences?.getString("lat", "")?.toDoubleOrNull() ?: 0.0
-        val windSpeed = sharedPreferences?.getString("windSpeed", "")?.toDoubleOrNull() ?: 0.0
-        val windDirection = sharedPreferences?.getString("windDirection", "")?.toDoubleOrNull() ?: 0.0
-
-        // Wyświetl lub wykorzystaj wczytane dane pogodowe
-        // Możesz je np. przekazać do odpowiednich elementów interfejsu użytkownika
-
-        // Przykład użycia danych:
-        Log.d("checkShared", "Dla miasta $cityName - temperatura: $temperature °C, opis: $description $windSpeed, $windDirection")
-    }
-
-    private fun saveWeatherDataToSharedPreferences(
-        cityName: String,
-        temperature: Double,
-        pressure: Double,
-        description: String,
-        lon: Double,
-        lat: Double,
-        windSpeed: Double,
-        windDirection: Double
-    ) {
-        val sharedPreferences = activity?.getSharedPreferences(cityName, Context.MODE_PRIVATE)
-        val editor = sharedPreferences?.edit()
-
-        editor?.apply {
-            putString("temperature", temperature.toString())
-            putString("pressure", pressure.toString())
-            putString("description", description)
-            putString("lon", lon.toString())
-            putString("lat", lat.toString())
-            putString("windSpeed", windSpeed.toString())
-            putString("windDirection", windDirection.toString())
-            apply()
-        }
-
-
-    }
-
 
     private fun fetchWeatherData(cityName: String) {
+        Log.d("HomeFragmentNet", "Fetch")
         val apiKey = "54115490ba2f3c3c704b01a9e52dad7a"
         val url = "https://api.openweathermap.org/data/2.5/weather?q=$cityName&appid=$apiKey"
 
@@ -163,13 +103,7 @@ class Home : Fragment() {
                     saveWeatherDataToSharedPreferences(cityName, temperatureCelsius, pressure, description, lon, lat, windSpeed, windDirection)
 
                     requireActivity().runOnUiThread {
-                        binding.cityNameTextView.text = cityName
-                        binding.temperatureTextView.text = "$temperatureCelsius °C"
-                        binding.coordinatesTextView.text = "Współrzędne: $lat, $lon"
-                        binding.pressureTextView.text = "Ciśnienie: $pressure hPa"
-                        binding.descriptionTextView.text = "Opis: $description"
-                        binding.windSpeedTextView.text = "Prędkość wiatru: $windSpeed m/s"
-                        binding.windDirectionTextView.text = "Kierunek wiatru: $windDirection°"
+                        displayWeatherData(cityName, temperatureCelsius, pressure, description, lon, lat, windSpeed, windDirection)
                     }
                 } else {
                     requireActivity().runOnUiThread {
@@ -178,5 +112,61 @@ class Home : Fragment() {
                 }
             }
         })
+    }
+
+    private fun displayWeatherData(
+        cityName: String,
+        temperature: Double,
+        pressure: Double,
+        description: String,
+        lon: Double,
+        lat: Double,
+        windSpeed: Double,
+        windDirection: Double
+    ) {
+        // Wyświetl dane pogodowe w interfejsie użytkownika
+        binding.cityNameTextView.text = cityName
+        binding.temperatureTextView.text = "$temperature °C"
+        binding.coordinatesTextView.text = "Współrzędne: $lat, $lon"
+        binding.pressureTextView.text = "Ciśnienie: $pressure hPa"
+        binding.descriptionTextView.text = "Opis: $description"
+        binding.windSpeedTextView.text = "Prędkość wiatru: $windSpeed m/s"
+        binding.windDirectionTextView.text = "Kierunek wiatru: $windDirection°"
+    }
+
+    private fun saveWeatherDataToSharedPreferences(
+        cityName: String,
+        temperature: Double,
+        pressure: Double,
+        description: String,
+        lon: Double,
+        lat: Double,
+        windSpeed: Double,
+        windDirection: Double
+    ) {
+        val sharedPreferences = activity?.getSharedPreferences(cityName, Context.MODE_PRIVATE)
+        val editor = sharedPreferences?.edit()
+
+        editor?.apply {
+            putString("temperature", temperature.toString())
+            putString("pressure", pressure.toString())
+            putString("description", description)
+            putString("lon", lon.toString())
+            putString("lat", lat.toString())
+            putString("windSpeed", windSpeed.toString())
+            putString("windDirection", windDirection.toString())
+            apply()
+        }
+    }
+
+    private fun addCityToFavorites(cityName: String) {
+        val sharedPreferences = activity?.getSharedPreferences("FavoriteCitiesPrefs", Context.MODE_PRIVATE)
+        val editor = sharedPreferences?.edit()
+
+        val favoriteCities = sharedPreferences?.getStringSet("favoriteCities", HashSet())?.toMutableSet() ?: mutableSetOf()
+        favoriteCities.add(cityName)
+
+        editor?.putStringSet("favoriteCities", favoriteCities)?.apply()
+        Log.d("FavoriteCitiesXD", "Ulubione miasta: $favoriteCities")
     }
 }
