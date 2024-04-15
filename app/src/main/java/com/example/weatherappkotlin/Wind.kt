b9c0eac4bcfe19be417ed1e4a0d1b7aed73b7509
Jakub Kozlowski <jakub.kozlowski1104@gmail.com>
2024-04-15 07:48:15 +0200

    Update Wind fragment

--- app/src/main/java/com/example/weatherappkotlin/Wind.kt
+++ app/src/main/java/com/example/weatherappkotlin/Wind.kt
@@ -6,40 +6,67 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import com.example.weatherappkotlin.databinding.FragmentWindBinding
 import com.google.gson.Gson
 import com.google.gson.JsonObject
-import okhttp3.Call
-import okhttp3.Callback
-import okhttp3.OkHttpClient
-import okhttp3.Request
+import okhttp3.*
 import java.io.IOException
 
 class Wind : Fragment() {
     private lateinit var binding: FragmentWindBinding
+    private lateinit var networkConnection: NetworkConnection
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentWindBinding.inflate(inflater, container, false)
-
-        val sharedPreferences = activity?.getSharedPreferences("CityWeatherPrefs", Context.MODE_PRIVATE)
-        val cityName = sharedPreferences?.getString("cityName", "") ?: ""
-        Log.d("WindFragmentCheck", "Odczytano nazwę miasta: $cityName")
-
-        if (cityName.isNotEmpty()) {
-            fetchWeatherData(cityName)
-        }
-
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        val sharedPreferences = activity?.getSharedPreferences("CityWeatherPrefs", Context.MODE_PRIVATE)
+        val cityName = sharedPreferences?.getString("cityName", "") ?: ""
+
+        networkConnection = NetworkConnection(requireContext())
+        networkConnection.observe(viewLifecycleOwner, Observer { isConnected ->
+            if (isConnected) {
+                Log.d("WindFragmentNet", "Internet connected")
+                // Pobierz dane z API, gdy jest dostęp do Internetu
+                binding.networkStatusTextView.visibility = View.GONE
+                binding.refreshData.visibility = View.VISIBLE
+                if (cityName.isNotEmpty()) {
+                    fetchWeatherData(cityName)
+                }
+                binding.refreshData.setOnClickListener {
+                    if (cityName.isNotEmpty()) {
+                        fetchWeatherData(cityName)
+                        Toast.makeText(context, "Dane zostały odświeżone", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            } else {
+                Log.d("WindFragmentNet", "No internet connection")
+                binding.networkStatusTextView.visibility = View.VISIBLE
+                binding.refreshData.visibility = View.GONE
+                // Odczytaj dane z SharedPreferences, gdy brak dostępu do Internetu
+                val sharedPreferences = activity?.getSharedPreferences(cityName, Context.MODE_PRIVATE)
+                val windSpeed = sharedPreferences?.getString("windSpeed", "")?.toDoubleOrNull() ?: 0.0
+                val windDirection = sharedPreferences?.getString("windDirection", "")?.toDoubleOrNull() ?: 0.0
+
+                Log.d("WindFragmentNet", "Shared")
+                // Wyświetl dane z SharedPreferences
+                displayWeatherData(cityName, windSpeed, windDirection)
+            }
+        })
+    }
 
     private fun fetchWeatherData(cityName: String) {
+        Log.d("WindFragmentNet", "Fetch")
         val apiKey = "54115490ba2f3c3c704b01a9e52dad7a"
         val url = "https://api.openweathermap.org/data/2.5/weather?q=$cityName&appid=$apiKey"
 
@@ -54,7 +81,7 @@ class Wind : Fragment() {
                 }
             }
 
-            override fun onResponse(call: Call, response: okhttp3.Response) {
+            override fun onResponse(call: Call, response: Response) {
                 if (response.isSuccessful) {
                     val gson = Gson()
                     val jsonObject = gson.fromJson(response.body?.string(), JsonObject::class.java)
@@ -62,9 +89,11 @@ class Wind : Fragment() {
                     val windSpeed = jsonObject.getAsJsonObject("wind").get("speed").asDouble
                     val windDirection = jsonObject.getAsJsonObject("wind").get("deg").asDouble
 
+                    // Zapisz dane pogodowe do SharedPreferences dla danego miasta
+                    saveWeatherDataToSharedPreferences(cityName, windSpeed, windDirection)
+
                     requireActivity().runOnUiThread {
-                        binding.windSpeedTextView.text = "Prędkość wiatru: $windSpeed m/s"
-                        binding.windDirectionTextView.text = "Kierunek wiatru: $windDirection°"
+                        displayWeatherData(cityName, windSpeed, windDirection)
                     }
                 } else {
                     requireActivity().runOnUiThread {
@@ -73,5 +102,29 @@ class Wind : Fragment() {
                 }
             }
         })
+    }
+
+    private fun displayWeatherData(
+        cityName: String,
+        windSpeed: Double,
+        windDirection: Double
+    ) {
+        binding.windSpeedTextView.text = "Prędkość wiatru: $windSpeed m/s"
+        binding.windDirectionTextView.text = "Kierunek wiatru: $windDirection°"
+    }
+
+    private fun saveWeatherDataToSharedPreferences(
+        cityName: String,
+        windSpeed: Double,
+        windDirection: Double
+    ) {
+        val sharedPreferences = activity?.getSharedPreferences(cityName, Context.MODE_PRIVATE)
+        val editor = sharedPreferences?.edit()
+
+        editor?.apply {
+            putString("windSpeed", windSpeed.toString())
+            putString("windDirection", windDirection.toString())
+            apply()
+        }
     }
 }
